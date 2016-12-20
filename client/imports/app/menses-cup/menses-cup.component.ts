1520f8c00abe6e87b4ecf8573a07585bcc6cfa91
tadkim <icanhear@dgu.edu>
2016-12-20 23:23:15 +0900

    feat(add): indexlist mock data

--- client/imports/app/menses-cup/menses-cup.component.ts
+++ client/imports/app/menses-cup/menses-cup.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Index } from '../index.model';
-import { IndexList } from '../mock-indexlist';
+// import { IndexList } from '../mock-indexlist';`
 
 import { McIntroComponent } from './mc-intro/mc-intro.component';
 import { McShapeComponent } from './mc-shape/mc-shape.component';
@@ -25,16 +25,30 @@ import gridStyle from '../../../../public/typo.css';
 
 export class MensesCupComponent {
         currentSection: string = '생리컵';
-        indexData: any;
         selectedIndex: Index;
+
+        IndexList = [
+            { name: '생리컵', name_eng: 'intro', endScroll: '#intro' },
+            { name: '생리컵이란?', name_eng: 'intro', endScroll: '#intro' },
+            { name: '제품정보', name_eng: 'info', endScroll: '#info' },
+            { name: '역사', name_eng: 'history', endScroll: '#history' },
+            { name: '장단점', name_eng: 'proscons', endScroll: '#proscons' },
+            { name: '생김새', name_eng: 'shape', endScroll: '#shape' },
+            { name: '사용법', name_eng: 'usage', endScroll: '#usage' }
+        ];
         
         
         constructor() { }
 
         ngOnInit(): void{
             
-            this.selectedIndex = { name: '인트로', name_eng: 'intro', endScroll: '#intro' };
+            this.selectedIndex = { name: '생리컵', name_eng: 'intro', endScroll: '#intro' };
             
+        }
+    
+        onSelect(index):void {
+            console.log(index);
+            this.selectedIndex = index;
         }
        
         
