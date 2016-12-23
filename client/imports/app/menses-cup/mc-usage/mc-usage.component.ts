fc17fa4d80a154b15bd7e71fce5c4eb573c2171c
tadkim <icanhear@dgu.edu>
2016-12-24 01:58:32 +0900

    feat(add): change stylesheet plain css to scss

--- client/imports/app/menses-cup/mc-usage/mc-usage.component.ts
+++ client/imports/app/menses-cup/mc-usage/mc-usage.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { WindowRef } from '../../WindowRef';
 
 import gridStyles from '../../../../../public/typo.css';
 import template from './mc-usage.component.html';
@@ -10,98 +9,45 @@ import template from './mc-usage.component.html';
         gridStyles
 })
 export class McUsageComponent {
-        modal: any = {};
-        windowObj: Object;
+        title: string = '생리컵 사용법';
+        modalState: boolean = false;
+        cardModel: any;
+        fold1_mouseover: boolean = true;
 
 
-        constructor(private windowref: WindowRef) {
-                console.log('windowRef is', windowref.nativeWindow);
-         }
-        
-        // 이벤트 리스너를 추가하는 핼퍼 함수
-        addEvent (el, event, callback) {
-          if ('addEventListener' in el) {                  // addEventListener 메서드가 지원되면
-            el.addEventListener(event, callback, false);   // 그대로 사용한다
-          } else {                                         // 그렇지 않으면
-            el['e' + event + callback] = callback;         // IE용 대체 코드를 작성한다
-            el[event + callback] = function () {
-              el['e' + event + callback](window.event);
-            };
-            el.attachEvent('on' + event, el[event + callback]);
-          }
+        onChangeState(e) {
+                console.log(e);
+                this.fold1_mouseover = (this.fold1_mouseover !== true) ? true : false;
         }
 
-        
-        // 이벤트 리스너를 제거하는 핼퍼 함수
-        removeEvent (el, event, callback) {
-          if ('removeEventListener' in el) {                      
-            el.removeEventListener(event, callback, false);       // removeEventListener 메서드가 지원되면 그대로 사용한다
-          } else {                                                // 그렇지 않으면
-            el.detachEvent('on' + event, el[event + callback]);   // IE용 대체 코드를 작성한다
-            el[event + callback] = null;
-            el['e' + event + callback] = null;
-          }
+        questionList = ['난이도', '평가항목2', '평가항목3', '평가항목4', '평가항목5'];
+
+        getCardData(): any {
+                return [
+                        { index:0, name: 'c자형 접기', imgUrl: '#' }
+
+                ];
         }
-        
-        //modal init
-        initModal(): void{
+        onOpen(): void{
+                this.modalState = true;
+        }
+        onClose(): void {
+                this.modalState = false;
+        }
+
+        onCard(e): void{
+                console.log(e);
+                this.modalState = true;
+        }
+        onMouseover(e): void {
                 
-
-                //모달창을 열기위한 클릭이벤트
-                let openModal = document.getElementById('share');
+                
+                console.log(e);
+                
+        }
         
-                this.addEvent(openModal, 'click', function (e) {
-                        console.log('click in openModal!');
-                });
-        }
-        basicModal(): void {
-                let $window = this.windowObj;
-                let $modal = document.createElement('div');
-                let $content = document.createElement('div');
-                let $close = document.createElement('button');
-
-
-                //class, attribute 설정
-                $modal.className = "modal";
-                $content.className = "modal-content";
-                $close.className = "modal-close";
-                $close.setAttribute("role", "button");
-                
-
-                //modal 요소하위에 content, close 요소 추가
-                $modal.appendChild($content);
-                $modal.appendChild($close);
-
-                
-                this.modal = function () {
-                       
-
-                        this.addEvent($close, 'on', function (e) {
-                                console.log('close event!');
-                                e.preventDefault();
-                                // this.modal.close();
-                        });
-                        console.log("$window.height()");
-                        console.log($window);
-/*
-
-                        return {
-                                center(): void {
-                                        let top = Math.max($window.height() - $modal.outer)
-                                }
-                        }
-
-*/                        
-
-                        
-                }; //modal end
-        }
         ngOnInit(): void {
-                this.windowObj = this.windowref.nativeWindow; //Window객체를 변수에 할당
-                this.initModal();
-                this.basicModal();
-                this.modal();
-
+               
                 
         }
         
